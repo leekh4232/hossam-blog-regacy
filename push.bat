7c1265059b2c55c7d92f9452f3c71658f3817111
이광호 <leekh4232@gmail.com>
2023-05-24 11:23:41 +0900

    update 2023-05-24 11:23:41.77

--- push.bat
+++ push.bat
@@ -1,14 +1,21 @@
 @echo off
 cls
 
+
+IF "%1" == "" (
+    SET comment=update %DATE% %TIME%
+) ELSE (
+    SET comment=%1
+)
+
 echo $ git add --all
 git add --all
 
 echo.
 
-echo $ git commit -m "update %DATE% %TIME%"
+echo $ git commit -m "%comment%"
 echo ----------------------------------------
-git commit -m "update %DATE% %TIME%"
+git commit -m "%comment%"
 
 echo.
 
